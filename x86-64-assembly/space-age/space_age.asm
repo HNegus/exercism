075249bb8ba20f8918ce4e27622813c547150efb
HNegus <ik@henok.nl>
2023-06-15 16:03:54 +0200

    finish space_age

--- x86-64-assembly/space-age/space_age.asm
+++ x86-64-assembly/space-age/space_age.asm
@@ -1,29 +1,32 @@
-section .data
-; three: dd 3.0e1
-three: dq 3
+
+[default rel]
 
 section .text
 global age
 age:
     push rbp
-    mov rax, qword three
-    pop rbp
+    mov rbp, rsp
+    ; rdi = int index
+    ; rsi = int seconds
+    lea rax, [mercury_year]     ; load base array index
+    movss xmm1, [rax + rdi*4]   ; load year array value
+    mulss xmm1, [earth_seconds] ; multiply by earth seconds
+    cvtsi2ss xmm0, qword rsi    ; convert argument into float
+    divss xmm0, xmm1            ; seconds / (earth_seconds * planet_year)
+    leave
     ret
 
 
-; four: dq 4.0
-
-; earth_seconds:  dd 31557600
-; mercury_year:   dd 0.2408467
-; venus_year:     dd 0.61519726
-; earth_year:     dd 1.0
-; mars_year:      dd 1.8808158
-; jupiter_year:   dd 11.862615
-; saturn_year:    dd 29.447498
-; uranus_year:    dd 84.016846
-; neptune_year:   dd 164.79132
-
-
+section .data
+earth_seconds:  dd 31557600.0
+mercury_year:   dd 0.2408467
+venus_year:     dd 0.61519726
+earth_year:     dd 1.0
+mars_year:      dd 1.8808158
+jupiter_year:   dd 11.862615
+saturn_year:    dd 29.447498
+uranus_year:    dd 84.016846
+neptune_year:   dd 164.79132
 
 %ifidn __OUTPUT_FORMAT__,elf64
 section .note.GNU-stack noalloc noexec nowrite progbits
