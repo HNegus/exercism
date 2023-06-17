ea80a097b41eb0e59442af1e0728e512c47de0f8
HNegus <ik@henok.nl>
2023-06-17 15:02:27 +0200

    2x faster

--- x86-64-assembly/rotational-cipher/rotational_cipher.asm
+++ x86-64-assembly/rotational-cipher/rotational_cipher.asm
@@ -1,46 +1,49 @@
 section .text
 global rotate
 rotate:
-    ; rdi = const char *text
-    ; rsi = int shift_key
-    ; rcx = char *buffer
-    ; r8 = char letter
-    ; r9 = offset
-    ; r10 = 26 // number of letters
-    mov rcx, rdx ; move buffer to rcx
-    mov r10, 26
+    ; r10 = const char *text
+    ; esi = int shift_key
+    ; r11 = char *buffer
+    ; ebx = char letter
+    ; edi = offset
+    ; ecx = 26 // number of letters
+    push rbx
+    mov r10, rdi
+    mov r11, rdx ; move buffer to r11
+    mov ecx, 26
 check_char:
-    movzx r8, byte [rdi] ; get current character
-    cmp r8, 'A'          ; char to limits and jump accordingly
+    movzx ebx, byte [r10] ; get current character
+    cmp ebx, 'A'          ; char to limits and jump accor10ngly
     jl copy_char
-    cmp r8, 'Z'
+    cmp ebx, 'Z'
     jle capital_letter
-    cmp r8, 'a'
+    cmp ebx, 'a'
     jl copy_char
-    cmp r8, 'z'
+    cmp ebx, 'z'
     jg copy_char
 non_capital_letter:
-    mov r9, 'a'     ; offset = 'a'
+    mov edi, 'a'     ; offset = 'a'
     jmp shift
 capital_letter:
-    mov r9, 'A'     ; offset = 'A'
+    mov edi, 'A'     ; offset = 'A'
 shift:
-    sub r8, r9      ; remove offset
-    add r8, rsi     ; add shift
-    xor rdx, rdx    ; zero out for division
-    mov rax, r8
-    div r10          ; divide char by 26
-    mov r8, rdx      ; set char to remainder
-    add r8, r9
+    sub ebx, edi      ; remove offset
+    add ebx, esi     ; add shift
+    xor edx, edx    ; zero out for division
+    mov eax, ebx
+    div ecx          ; divide char by 26
+    mov ebx, edx      ; set char to remainder
+    add ebx, edi
 copy_char:
-    mov byte [rcx], r8b ; copy encrypted letter to buffer
+    mov byte [r11], bl ; copy encrypted letter to buffer
 increment_buffers:
-    inc rdi             ; move to next character
-    inc rcx
-    cmp byte [rdi], 0   ; check if at end of string
+    inc r10             ; move to next character
+    inc r11
+    cmp byte [r10], 0   ; check if at end of string
     jne check_char
 done:
-    mov byte  [rcx], 0
+    mov byte  [r11], 0
+    pop rbx
     ret
 
 section .bss
